f93399c6d894b65fba4fd1980cb23f48ed401280
lafith <mmlafith3@gmail.com>
2021-04-16 02:12:14 +0530

    feat(template): add about us page
    a nav bar is included in layout.html with link for about us page

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,16 @@ from flask import render_template
 from app import lbms_app
 
 
+# This route is for showing 'Home' page
 @lbms_app.route('/')
 @lbms_app.route('/index')
 def index():
     """View function for home page"""
     return render_template('home.html')
+
+
+# This route is for showing 'About Us' type information
+@lbms_app.route('/about')
+def about():
+    """View function for About Us page"""
+    return render_template('about.html')
