3e02f67598cdae16d7831f13722ab8fe3b0846d8
lafith <mmlafith3@gmail.com>
2021-04-18 01:10:34 +0530

    style(template): use bootstrap-flask instead of cdn

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from flask_bootstrap import Bootstrap
 
 
 lbms_app = Flask(__name__)
 lbms_app.config.from_object(Config)
-
+Bootstrap(lbms_app)
 # database setup
 db = SQLAlchemy(lbms_app)
 migrate = Migrate(lbms_app, db)
--- config.py
+++ config.py
@@ -7,3 +7,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'lbms.db')
+    BOOTSTRAP_SERVE_LOCAL=True
+
